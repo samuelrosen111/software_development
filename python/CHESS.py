d13ce1a3a0eaddc04a7319852c6670b0653b2f77
samuelrosen111 <samuelrosen121@gmail.com>
2024-02-01 14:14:23 +0100

    basic chess rules implemented

--- python/CHESS.py
+++ python/CHESS.py
@@ -11,6 +11,10 @@ class Piece:
         self.name = name
         self.short_name = short_name
 
+    # Add get color function
+    def get_color(self):
+        return self.color
+
     def __str__(self):
         return f"Piece(name={self.name}, short_name={self.short_name}, color={self.color}, row={self.row}, col={self.col})"
 
@@ -41,7 +45,6 @@ def createBoard():
     board[7][7] = Piece(7, 7, "black", "rook", "b_R")
 
     return board
-
 
 def printBoard(board):
     # Define the square frame and empty square representation
@@ -131,11 +134,218 @@ def movePiece(board, player_to_move):
         if board[row_position][col_position] is None:
             print("There is no piece at the starting square. Please try again.")
             continue
-
+        if(move_is_not_legal(board, row_position, col_position, row_target, col_target, player_to_move)):
+            print("That is not a legal move (see reason above). Please try again.")
+            continue
         # Move the piece
         board[row_target][col_target] = board[row_position][col_position]
         board[row_position][col_position] = None
         return board
+
+def move_is_not_legal(board, row_position, col_position, row_target, col_target, player_to_move):
+    # AUXILLIARY functions used to check moves for each piece
+
+    def rook_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        # Check if the move is horizontal or vertical
+            if not (row_position == row_target or col_position == col_target):
+                error_msg = "The rook can only move horizontally or vertically."
+                return True, error_msg
+            # If rook moves, check if there are any pieces in between
+            if(row_position == row_target):
+                # Check if there are any pieces in between
+                if(col_position < col_target):
+                    for i in range(col_position + 1, col_target):
+                        if(board[row_position][i] is not None):
+                            error_msg = "There is a piece in between the rook and the target square."
+                            return True, error_msg
+                else:
+                    for i in range(col_target + 1, col_position):
+                        if(board[row_position][i] is not None):
+                            error_msg = "There is a piece in between the rook and the target square."
+                            return True, error_msg
+            # Check that rook is moving vertically
+            else:
+                # Check if there are any pieces in between
+                if(row_position < row_target):
+                    for i in range(row_position + 1, row_target):
+                        if(board[i][col_position] is not None):
+                            error_msg = "There is a piece in between the rook and the target square."
+                            return True, error_msg
+                else:
+                    for i in range(row_target + 1, row_position):
+                        if(board[i][col_position] is not None):
+                            error_msg = "There is a piece in between the rook and the target square."
+                            return True, error_msg
+            return False, "Legal Move"
+
+    def knight_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        # Check if the move is L-shaped
+        if not ((abs(row_position - row_target) == 2 and abs(col_position - col_target) == 1) or (abs(row_position - row_target) == 1 and abs(col_position - col_target) == 2)):
+            message = "The knight can only move in an L-shape."
+            return True, message
+        return False, "Legal Move"
+
+    def bishop_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        delta_row = row_target - row_position
+        delta_col = col_target - col_position
+        # Check if the move is diagonal
+        if not (abs(delta_row) == abs(delta_col)):
+            message = "The bishop can only move diagonally."
+            return True, message
+        # Given diagonal move check if there are any pieces in between
+        if(delta_row > 0 and delta_col > 0):
+            for i in range(1, delta_row):
+                if(board[row_position + i][col_position + i] is not None):
+                    message = "There is a piece in between the bishop and the target square."
+                    return True, message
+        elif(delta_row > 0 and delta_col < 0):
+            for i in range(1, delta_row):
+                if(board[row_position + i][col_position - i] is not None):
+                    message = "There is a piece in between the bishop and the target square."
+                    return True, message
+        elif(delta_row < 0 and delta_col > 0):
+            for i in range(1, abs(delta_row)):
+                if(board[row_position - i][col_position + i] is not None):
+                    message = "There is a piece in between the bishop and the target square."
+                    return True, message
+        else:
+            for i in range(1, abs(delta_row)):
+                if(board[row_position - i][col_position - i] is not None):
+                    message = "There is a piece in between the bishop and the target square."
+                    return True, message
+        return False, "Legal Move"
+
+    def queen_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        # Begin by checking if move is like a rook, bishop or none of the above
+        delta_row = row_target - row_position
+        delta_col = col_target - col_position
+        # Check if the move is diagonal
+        if (abs(delta_row) == abs(delta_col)): 
+            # Given a diagonal move use the bishop function to check if there are any pieces in between
+            is_illegal, msg = bishop_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+            #Edit message to be for queen, not bishop
+            msg = msg.replace("bishop", "queen")
+            return is_illegal, msg
+        # Check if the move is horizontal or vertical (rook)
+        elif (row_position == row_target or col_position == col_target):
+            # Queen now moves like a rook, use the rook function to check if there are any pieces in between
+            is_illegal, msg = rook_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+            #Edit message to be for queen, not rook
+            msg = msg.replace("rook", "queen")
+            return is_illegal, msg
+        else:
+            message = "The queen can only move diagonally, horizontally or vertically."
+            return True, message
+        return False, "Legal Move"
+
+    def king_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        delta_row = row_target - row_position
+        delta_col = col_target - col_position
+        # Check that the move is only one square in any direction
+        if not (abs(delta_row) <= 1 and abs(delta_col) <= 1):
+            message = "The king can only move one square in any direction."
+            return True, message
+        return False, "Legal Move"
+
+    def pawn_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move):
+        delta_row = row_target - row_position
+        delta_col = col_target - col_position
+        # White pawn
+        if(player_to_move == "white"):
+            # Side capture is always legal
+            if(abs(delta_col) == 1 and delta_row == 1 and board[row_target][col_target].color == "black"):
+                return False, "Legal Move"
+            # If pawn is in starting position it can move two squares forward or one square forward
+            if(delta_col==0 and delta_row==2 or delta_col==0 and delta_row==1):
+                return False, "Legal Move"
+            # If pawn is not in starting position it can only move one square forward
+            if(delta_col==0 and delta_row==1):
+                return False, "Legal Move"
+            else:
+                message = "The pawn can only move one square forward, two squares forward from starting position or one square diagonally to capture."
+                return True, message
+            # We will ignore en-passant for now
+            #TODO: Implement en-passant
+        # Black pawn
+        else:
+            # Side capture is always legal
+            if(abs(delta_col) == 1 and delta_row == -1 and board[row_target][col_target].color == "white"):
+                return False, "Legal Move"
+            # If pawn is in starting position it can move two squares forward or one square forward
+            if(delta_col==0 and delta_row==-2 or delta_col==0 and delta_row==-1):
+                return False, "Legal Move"
+            # If pawn is not in starting position it can only move one square forward
+            if(delta_col==0 and delta_row==-1):
+                return False, "Legal Move"
+            else:
+                message = "The pawn can only move one square forward, two squares forward from starting position or one square diagonally to capture."
+                return True, message
+            # We will ignore en-passant for now
+
+
+    # There are already some checks in place from the movePiece function, they are: 
+    # 1) The starting square is not empty
+    # 2) The target square is not the same as the starting square
+    # 3) The starting square contains a piece of the player's color
+    change_in_row = row_target - row_position # Auxiliary variables to check if the move is horizontal, vertical or diagonal
+    change_in_col = col_target - col_position # Auxiliary variables to check if the move is horizontal, vertical or diagonal
+
+    piece_to_move = board[row_position][col_position]
+    # Basic checks:
+    # Check if target square is occupied by a piece of the same color
+    if(board[row_target][col_target] is not None):
+        if board[row_target][col_target].get_color() == piece_to_move.get_color():
+            print("The target square is occupied by a piece of the same color.")
+            return True # Illegal move
+    
+    # ROOK ------------------------------------------------------------------------------------------------------------------------
+    if(piece_to_move.name == "rook"):
+        is_illegal, msg = rook_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+    # KNIGHT ------------------------------------------------------------------------------------------------------------------------
+    elif(piece_to_move.name == "knight"):
+        is_illegal, msg = knight_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+    # BISHOP ------------------------------------------------------------------------------------------------------------------------
+    elif(piece_to_move.name == "bishop"):
+        # Uses the auxiliary variables to check if the move is diagonal
+        is_illegal, msg = bishop_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+
+    # QUEEN ------------------------------------------------------------------------------------------------------------------------
+    elif(piece_to_move.name == "queen"):
+        # Basic check to see if move is diagonal, horizontal or vertical
+        is_illegal, msg = queen_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+    # KING ------------------------------------------------------------------------------------------------------------------------
+    elif(piece_to_move.name == "king"):
+        # Check if the move is only one square in any direction
+        is_illegal, msg = king_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+    # PAWN ------------------------------------------------------------------------------------------------------------------------
+    elif(piece_to_move.name == "pawn"):
+        # Check if the move is only one square in any direction
+        is_illegal, msg = pawn_move_is_illegal(board, row_position, col_position, row_target, col_target, player_to_move)
+        if(is_illegal):
+            print(msg)
+            return True
+    # If none of the above checks are triggered the move is legal
+    else:
+        print("The piece you are trying to move is not a legal chess piece. This error message should not appear - if it does the program has an error to it.")
+        return True
+        
+    
+    return False # If all checks are passed it is a legal move
 
 def main():
     player_to_move = "white"
@@ -145,6 +355,7 @@ def main():
 
     # Start the game loop for moving pieces
     while True:
+        print("It is " + player_to_move + "'s turn to move.")
         board = movePiece(board, player_to_move)
         printBoard(board)
 
